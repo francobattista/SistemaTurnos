03d33b80ef671b18f118238ef55da06422f41a1a
Franco Battista <89143582+francobattista@users.noreply.github.com>
2022-11-18 13:04:53 -0300

    Update navbar.controller.js

--- SistemaTurnos/Frontend/js/controllers/navbar.controller.js
+++ SistemaTurnos/Frontend/js/controllers/navbar.controller.js
@@ -3,8 +3,9 @@ import { createAuthClientLogOut } from "../services/auth.service.js";
 export default () => {
     const view = `
     <header style="background-color:lightgreen;" class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <b style="font-family:cursive; font-size:20px; margin-left:20px" >Primeiro a armadilha, depois o queijo #AlfaLovers</b>
-        <img src="./../../assets/Alfa.jpg" style="width:75px;height:50px">
+        
+        <!-- <b style="font-family:cursive; font-size:20px; margin-left:20px" >Primeiro a armadilha, depois o queijo #AlfaLovers</b>
+        <img src="./../../assets/Alfa.jpg" style="width:75px;height:50px"> -->
         <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
         </a>
